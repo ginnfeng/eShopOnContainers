94de23be494607ccbdd9062c2d4de76083534353
Feng <ginnfeng@yahoo.com>
2020-07-09 09:33:25 +0800

    20200709-0

---
+++ tutorial/APIDemo/BuildingBlocks/Common/Common.Support/Support/ErrorHandling/RetryHelper.cs
@@ -0,0 +1,41 @@
+﻿////*************************Copyright © 2020 Feng 豐**************************	
+// Created    : 7/8/2020 2:59:51 PM 
+// Description: RetryHelper.cs  
+// Revisions  :            		
+// **************************************************************************** 
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Support.ErrorHandling
+{
+    public class RetryHelper
+    {
+        static public readonly TimeSpan defaultRetryTimeSpan = TimeSpan.FromSeconds(2);
+        static public T AutoRetry<T>(Func<T> func, int retryCount = 1)
+        {
+            return AutoRetry<T>(func, defaultRetryTimeSpan, retryCount);
+        }
+        static public T AutoRetry<T>(Func<T> func, TimeSpan ts, int retryCount = 1)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception err)
+            {
+                if (retryCount <= 0) throw err;
+                System.Threading.Thread.Sleep(ts);
+                return AutoRetry<T>(func, ts, retryCount - 1);
+            }
+        }
+        static public void AutoRetry(Action act, int retryCount = 1)
+        {
+            AutoRetry(act, defaultRetryTimeSpan, retryCount);
+        }
+        static public void AutoRetry(Action act, TimeSpan ts, int retryCount = 1)
+        {
+            AutoRetry<bool>(() => { act(); return true; }, defaultRetryTimeSpan, retryCount);;
+        }
+    }
+}
---
+++ tutorial/APIDemo/UTool/Test/Test_Exception.cs
@@ -0,0 +1,47 @@
+﻿////*************************Copyright © 2020 Feng 豐**************************	
+// Created    : 7/8/2020 2:49:24 PM 
+// Description: Test_Exception.cs  
+// Revisions  :            		
+// **************************************************************************** 
+using Support.ErrorHandling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UTDll;
+namespace UTool.Test
+{
+    class Test_Exception : UTest
+    {
+        public Test_Exception()
+        {
+            //
+            // TODO: Add constructor logic here
+            //      
+        }
+        
+        [UMethod]
+        public void T_Retry()
+        {// TODO: Add Testing logic here
+            var xo = new OutService();
+            var ts=new TimeSpan(0, 0, 3);//Retry 間隔時間
+            int retryNum = 2;//Retry 之次數限制
+            try
+            {
+                RetryHelper.AutoRetry(() => xo.CallWebServiceFunction(), ts, retryNum);
+            }
+            catch (Exception e)
+            {
+                //Here! To Log Error!
+                throw e;
+            }
+            
+        }
+    }
+    class OutService
+    {
+        public void CallWebServiceFunction()
+        {
+            //......呼叫介面，其可能throw Exception;
+        }
+    }
+}

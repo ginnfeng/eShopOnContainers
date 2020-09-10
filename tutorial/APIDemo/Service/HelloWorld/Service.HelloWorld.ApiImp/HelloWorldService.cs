87c60c69167b1a867cfc9c89cdc14111c4c55571
Feng <ginnfeng@yahoo.com>
2020-09-10 12:00:13 +0800

    20200910

--- tutorial/APIDemo/Service/HelloWorld/Service.HelloWorld.ApiImp/HelloWorldService.cs
+++ tutorial/APIDemo/Service/HelloWorld/Service.HelloWorld.ApiImp/HelloWorldService.cs
@@ -25,7 +25,7 @@ namespace Service.HelloWorld.ApiImp
         }
         public string Hello(string id)
         {
-            return $"*** Hello {id}";           
+            return $"### Hello {id}";           
         }
         public IEnumerable<HelloWeather> DefaultGet()
         {
@@ -47,22 +47,22 @@ namespace Service.HelloWorld.ApiImp
                 Date = inp.Date,
                 UserName = inp.UserName,
                 TemperatureC = rng.Next(-20, 55),
-                Summary = $"*** id1={id1} id2={id2}  id3={id3} "
+                Summary = $"$### id1={id1} id2={id2}  id3={id3} "
             };
         }
 
         public string HelloGet(string id1, string id2)
         {
-            return $"*** HelloGet('{id1},{id2}')";
+            return $"$##23* HelloGet('{id1},{id2}')";
         }
 
         public string HelloPost(string id1, string id2)
         {
-            return $"*** HelloPost('{id1},{id2}')";
+            return $"$### HelloPost('{id1},{id2}')";
         }
         public void OneWayCall(string id1, HelloInput inp)
         {
-            Debug.WriteLine($"*** ConsoleMessage (HelloMQDemo {id1}) ");
+            Debug.WriteLine($"$### ConsoleMessage (HelloMQDemo {id1}) ");
             //var ts = new JsonNetTransfer();
             //ts.Save(inp, @"d:\temp\a.json");
         }
@@ -76,7 +76,7 @@ namespace Service.HelloWorld.ApiImp
                 UserName = id1,
                 TemperatureC = rng.Next(-20, 55)
             };
-            rlt.Summary = $"*** from {nameof(TwoWayCall)}  {rlt.UserName} {rlt.TemperatureC} {rlt.Date}";
+            rlt.Summary = $"$### from {nameof(TwoWayCall)}  {rlt.UserName} {rlt.TemperatureC} {rlt.Date}";
             return new QuResult<HelloWeather> (rlt);
         }
     }
